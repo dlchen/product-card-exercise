5a4015de333375ff51d5db4b271587bb8a6a48e2
dlchen <dan.zot@gmail.com>
2020-06-01 12:16:01 -0700

    feat: add GET, POST /products/favorites

--- api/app.js
+++ api/app.js
@@ -3,12 +3,32 @@ const products = require('../src/products.json');
 
 const app = express();
 
+app.use(express.json());
+
 app.get('/products', (req, res) => {
-  return res.json(products);
+  return res.json(products.slice(0, 60));
 });
 
-app.get('/products/favorites', (req, res) => {
-  return res.json(products.slice(-10));
-});
+const mockFavorites = [
+  ...products.slice(0, 5).map((product) => product.variantId),
+  ...products.slice(10, 15).map((product) => product.variantId)
+];
+
+const addToFavorites = (productId) => {
+  mockFavorites.push(productId);
+}
+
+app.route('/products/favorites')
+  .get((req, res) => {
+    return res.json(mockFavorites);
+  })
+  .post((req, res) => {
+
+    // TODO: handle request validation
+    // TODO: handle errors
+
+    addToFavorites(req.body.productId);
+    return res.json(mockFavorites);
+  });
 
 module.exports = app;
